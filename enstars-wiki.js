b33fcb4adec6178b61553401c5b2e53082d5da13
Janis Rancourt <janis.rancourt@gmail.com>
2020-05-13 16:52:40 -0400

    cleaning code for release

--- enstars-wiki.js
+++ enstars-wiki.js
@@ -1,145 +1,33 @@
-// ==UserScript==
-// @name         enstars wiki story test code
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        https://ensemble-stars.fandom.com/*
-// @grant        none
-// ==/UserScript==
-
-(function() {
-    'use strict';
-
-//////////
-
-
-// I HAVE TO USE ES5 BECAUSE MEDIAWIKI IS OUTDATED AF WHY
-
-
-
-    
-console.log("script is running!111221");
-//anyone reading this just ignore the random numbers lmao
+console.log("Script version 1.0.0 is running! Contact XinChun93 on Twitter if there are any questions or concerns!");
 var d = document;
-
-
-// d.addEventListener("load",respondToPageLoad)
-
-// var tabContainers = d.querySelectorAll(".tabBody");
-    
-// tabContainers.forEach(defineTabBehavior);
-
-// function defineTabBehavior(tab,index,arr) {
-//     tab.addEventListener("DOMNodeInserted",handleTabLoad);
-//     console.log('added listener...');
-// }
-
-// d.querySelectorAll("li[data-tab*='flytabs']").forEach(function(tab){
-//     tab.addEventListener("click",function(){
-//         setTimeout(function(){ alert("Hello"); }, 5000);
-//     });
-// });
-
-// function handleTabLoad() {
-//     console.log("loaded new tab");
-// }
-
-
-
-
-
-
-/////actual working code below.....
-
-
-
-
-
-function jumpToStory() {
-    
-}
-
-
-// var jumpButton = d.createElement("button");
-// jumpButton.classList.add("jumpButton");
-// jumpButton.style.height="75px";
-// jumpButton.textContent="Jump to Story";
-// jumpButton.addEventListener("click",jumpToStory);
-
-
-
 var activator = d.createElement("button");
 activator.classList.add("activator");
 activator.style.height = "75px";
-activator.textContent="Activate Theme";
-activator.addEventListener("click",activateTheme);
-activator.setAttribute("id","themeJump")
-
-
+activator.textContent = "Activate Theme";
+activator.addEventListener("click", activateTheme);
+activator.setAttribute("id", "themeJump");
 var activatorJumpLink = d.createElement("a");
-activatorJumpLink.href="#flytabs_0-content-wrapper";
+activatorJumpLink.href = "#flytabs_0-content-wrapper";
 activatorJumpLink.appendChild(activator);
-
-
-
 var jumpToTop = d.createElement("button");
 jumpToTop.classList.add("jumpToTop");
 jumpToTop.style.height = "75px";
-jumpToTop.textContent="Jump To Top";
-
+jumpToTop.textContent = "Jump To Top";
 var jumpToTopLink = d.createElement("a");
-jumpToTopLink.href="#themeJump";
+jumpToTopLink.href = "#themeJump";
 jumpToTopLink.appendChild(jumpToTop);
-
-
-
-
-
 var body = d.querySelector("body");
-// body.appendChild(activator);
-
-
-// body.appendChild(jumpButtonLink);
-
- 
 body.style.position = "relative";
-// activator.style.position = "fixed";
-// activator.style.bottom = "0px";
-// activator.style.zIndex = "999";
 activator.style.width = "100%";
 jumpToTop.style.width = "100%";
-
-
-// jumpButtonLink.style.position="fixed";
-// jumpButtonLink.style.bottom="0px";
-// jumpButtonLink.style.left="100px";
-// jumpButtonLink.style.zIndex="999";
-
-
-
-// var tabBar = d.querySelector("#flytabs_0-content-wrapper");
 var tabBar = d.querySelector(".newStoryTable #flytabs_0");
-if(tabBar){
-tabBar.insertAdjacentElement("beforebegin",activatorJumpLink);
-tabBar.insertAdjacentElement("afterend",jumpToTopLink);
+if (tabBar) {
+  tabBar.insertAdjacentElement("beforebegin", activatorJumpLink);
+  tabBar.insertAdjacentElement("afterend", jumpToTopLink);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
 function activateTheme() {
- 
-var charArray = [
+  var charArray = [
     "Kohaku Oukawa",
     "Aira Shiratori",
     "Tomoya Mashiro",
@@ -193,309 +81,111 @@ var charArray = [
     "Jin Sagami",
     "Seiya Hidaka",
     "Anzu"
-];
- 
+  ];
 
-function removeBorders() {
+  function removeBorders() {
     var tableCells = d.querySelectorAll(".article-table td");
     tableCells.forEach(function(cell) {
-        cell.style.border = "none";
+      cell.style.border = "none";
     });
-}
-removeBorders();
+  }
+  removeBorders();
 
-
-function adjustImages() {
-
+  function adjustImages() {
     var storyImgs = d.querySelectorAll("img[data-image-name*='Render']");
- 
-    storyImgs.forEach(function(img,index) {
- 
-        img.style.maxWidth = "100px";
-        img.style.height = "auto";
-        img.classList.add("story-image");
- 
-        // console.log("find char name start")
- 
-        var charFileName = img.getAttribute("data-image-name");
-        // console.log("char file name is...")
-        // console.log(charFileName)
- 
-        var charName = findCharName(charFileName);
-        //console.log(`the character name found is ${charName}`)
- 
-
-var speakerNameClass = "speaker-name_" + index;
-var speakerNameClassSelector = "." + speakerNameClass;
-
-// var nameCheck = d.querySelector(speakerNameClassSelector);
-// console.log(nameCheck.classList);
-
-// console.log(d.querySelector(speakerNameClassSelector));
-if (!d.querySelector(speakerNameClassSelector)) {
-
+    storyImgs.forEach(function(img, index) {
+      img.style.maxWidth = "100px";
+      img.style.height = "auto";
+      img.classList.add("story-image");
+      var charFileName = img.getAttribute("data-image-name");
+      var charName = findCharName(charFileName);
+      var speakerNameClass = "speaker-name_" + index;
+      var speakerNameClassSelector = "." + speakerNameClass;
+      if (!d.querySelector(speakerNameClassSelector)) {
         var nameNode = d.createElement("p");
         nameNode.classList.add(speakerNameClass);
         nameNode.textContent = charName;
-
-
-
         if (window.matchMedia("(max-width: 700px)").matches) {
-            nameNode.style.textAlign = "left";
-            nameNode.textContent+=":";
+          nameNode.style.textAlign = "left";
+          nameNode.textContent += ":";
         }
-
-        img.insertAdjacentElement("afterend",nameNode);
- 
-
-}
-
-        //select td parent of image
-        // let parentTd = img.parentNode.parentNode.parentNode
-        var parentTd = img.closest("td");
-        var nextTd = parentTd.nextSibling;
-        if (nextTd) {
-            nextTd.classList.add("nextTd");
-        } else {
-            return;
-        }
- 
-        //need to account for edge cases in which
-        //there may be 2 images in one cell
-        //after this loop, find all td with 2 div class=center
-        //with the character images and then appropriately
-        //align them possibly stacked
- 
-        parentTd.style.verticalAlign = "top";
-        parentTd.style.paddingTop = "1em";
-        parentTd.style.fontSize = "1.5em";
-        parentTd.style.lineHeight = "1.2em";
- 
-        // console.log("showing contents")
- 
-        var dialogueBox = d.createElement("div");
-        dialogueBox.classList.add("dialigue-box");
- 
-        //also need to account for text content which is not contained in a p tag or other tag for some reason
- 
-            // let nonParagraphContent = nextTd.textContent
- 
-            // let newP = d.createElement("p")
-            // newP.textContent = nonParagraphContent
- 
-            // dialogueBox.appendChild(newP)
- 
- 
- 
-//potentially instead of this
-//with the above code making a new p
-//and the below code taking p tags,
-//just grab the inner html of the nextTd
-//and simply add that to the dialogueBox...
-//worth testing out...
-//// the above note has been implemented! obsolete code commented out
- 
- 
- 
- 
- 
-            // let contents = d.querySelectorAll(".nextTd > *")
-            // // console.log(contents)
- 
-            // contents.forEach(node => {
-            //     dialogueBox.appendChild(node)
-            // })
- 
- 
- 
-        var contents = nextTd.innerHTML;
- 
-        dialogueBox.innerHTML = contents;
- 
- 
- 
-        parentTd.appendChild(dialogueBox);
- 
-        nextTd.remove();
-
-
-        var parentRow = parentTd.parentNode;
-        // console.log(parentRow);
-        
-        //make every other row a different background color
-
-        if (index%2 ===0) {
-            parentRow.style.backgroundColor = "rgba(220,220,210,0.4)";
-        } else {
-            parentRow.style.backgroundColor = "rgba(215,215,220,0.6)";
-        }
-
-
-
-//remove borders
-        parentTd.style.border = "none";
-        parentRow.style.border = "none";
-
-
- 
-        parentTd.style.display = "flex";
-        parentTd.style.alignItems = "flex-start";
-        parentTd.style.justifyContent = "flex-start";
- 
-        if (window.matchMedia("(max-width: 700px)").matches) {
-            parentTd.style.flexDirection = "column";
-        }
-        if (window.matchMedia("(min-width: 701px)").matches) {
-            parentTd.style.flexDirection = "row";
-        }
- 
- 
-        //may need to adjust this part to make sure it
-        //will hit all images even if two or more are
-        //sharing a td initially
- 
-        var imageDiv = img.parentNode.parentNode;
-
-        if (window.matchMedia("(min-width: 701px)").matches) {
-            imageDiv.style.width = "150px";
-            imageDiv.style.flex = "0 0 150px";
-            dialogueBox.style.paddingLeft = "2em";
-        }
- 
+        img.insertAdjacentElement("afterend", nameNode);
+      }
+      //select td parent of image
+      var parentTd = img.closest("td");
+      var nextTd = parentTd.nextSibling;
+      if (nextTd) {
+        nextTd.classList.add("nextTd");
+      }
+      else {
+        return;
+      }
+      parentTd.style.verticalAlign = "top";
+      parentTd.style.paddingTop = "1em";
+      parentTd.style.fontSize = "1.5em";
+      parentTd.style.lineHeight = "1.2em";
+      // console.log("showing contents")
+      var dialogueBox = d.createElement("div");
+      dialogueBox.classList.add("dialigue-box");
+      var contents = nextTd.innerHTML;
+      dialogueBox.innerHTML = contents;
+      parentTd.appendChild(dialogueBox);
+      nextTd.remove();
+      var parentRow = parentTd.parentNode;
+      //make every other row a different background color
+      if (index % 2 === 0) {
+        parentRow.style.backgroundColor = "rgba(220,220,210,0.4)";
+      }
+      else {
+        parentRow.style.backgroundColor = "rgba(215,215,220,0.6)";
+      }
+      //remove borders
+      parentTd.style.border = "none";
+      parentRow.style.border = "none";
+      parentTd.style.display = "flex";
+      parentTd.style.alignItems = "flex-start";
+      parentTd.style.justifyContent = "flex-start";
+      if (window.matchMedia("(max-width: 700px)").matches) {
+        parentTd.style.flexDirection = "column";
+      }
+      if (window.matchMedia("(min-width: 701px)").matches) {
+        parentTd.style.flexDirection = "row";
+      }
+      var imageDiv = img.parentNode.parentNode;
+      if (window.matchMedia("(min-width: 701px)").matches) {
+        imageDiv.style.width = "150px";
+        imageDiv.style.flex = "0 0 150px";
+        dialogueBox.style.paddingLeft = "2em";
+      }
     });
-
-
-
-
-     if (window.matchMedia("(max-width: 700px)").matches) {
-        storyImgs.forEach(function(img) {
-            img.style.display = "none";
-        });
-     }
-
+    if (window.matchMedia("(max-width: 700px)").matches) {
+      storyImgs.forEach(function(img) {
+        img.style.display = "none";
+      });
+    }
     if (window.matchMedia("(min-width: 701px)").matches) {
-        storyImgs.forEach(function(img) {
-            img.style.display = "static";
-        });
-     }
-
-
- 
-}
- 
-adjustImages();
- 
- 
- 
-// Function for parsing file names for each character
- 
-function findCharName(file) {
+      storyImgs.forEach(function(img) {
+        img.style.display = "static";
+      });
+    }
+  }
+  adjustImages();
+  // Function for parsing file names for each character
+  function findCharName(file) {
     if (file.includes("Render")) {
-        //search for string of character's name with if/else statement
-        //and return their name without the rest of the filename.
-        //console.log(file)
- 
-        var name = "";
- 
- 
-        var parseName = function(file, chName) {
-            if (file.includes(chName)) {
-                name = chName;
-            }
-            return name;
-        };
- 
-        charArray.forEach(function(chName){
- 
-          if (parseName(file, chName)) {
-            // console.log(name);
-            return name;
-          }
- 
-        });
- 
+      var name = "";
+      var parseName = function(file, chName) {
+        if (file.includes(chName)) {
+          name = chName;
+        }
         return name;
- 
- 
-    } //else if other kind of render, card? character? need to double check...
-    //may not need to do this part bc i just reduced the
-    //requirement for matching to "Render" from "Dialogue Render" but we'll see, it works rn so imma leave it for now... yeah
- 
- 
+      };
+      charArray.forEach(function(chName) {
+        if (parseName(file, chName)) {
+          return name;
+        }
+      });
+      return name;
+    }
+  }
 }
- 
- 
- 
- 
- 
- 
-//still gotta consider how it loads on the pages with tabs
-//potentially have it wait for a tab to load
-//need to check how it functions when js is loaded directly on the wiki and not externally
-//possibly can store a user's preference in localstorage
-}
- 
- 
- 
- 
- 
- 
-//////////
-//future consisderation:
-//see if external images can be loaded when using the common.js
-//page on the wiki itself. unsure if that will work or be
-//allowed but it's worth at least seeing if it's doable or not.
- 
- 
- 
-//also try to separate into different functions,
-//particularly separating font size from the rest
-//and/or add a separate toggle to increase or decrease font size
- 
- 
-//also add some style changes like
-//less borders, more bg color changes, etc.
- 
-//also possibly font changes/options
- 
- 
- 
-//// also to do: either copy tabs to bottom of page, or
-//// add a jump to top button at the bottom --
-//// latter option may require adding an id to the body
-//// or another element which shows up high on the page
- 
- 
- 
- 
-// ALSO see if it's possible to load the script
-// simply upon loading/rendering/activating a tab
-// rather than having to do it based on a manual click
-// each time a new tab is loaded
-// note that it seems to be lazy loading the tabs
-// or something along those lines
-// so unsure if it's doable or not
-// but we can experiment and see
-// -- on a new branch ofc. that's a note to self
-// bc otherwise i will forget... lmao
- 
- 
- 
- 
-//it seems the script breaks reference links
-//and the jumping between the context and the explanation
-//at the bottom of the page
-//should look into what's causing it
-//and try to apply a fix...
-
-
-
-
-//////////
-    // Your code here...
-})();
-
-
-// Below is the file name format for the small circle icons, for reference.
-// [[File:Hokuto Hidaka Circle.png|center|90px]]<br>'''[[Hokuto Hidaka]]
-
